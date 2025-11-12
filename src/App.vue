7d51236bc07eee1e530ebe3873103d1489b562b0
Aditya Singh <saditya370@gmail.com>
2025-11-12 12:23:40 +0530

    Added vue router with home page and about page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1 class="logo">📝 My Vue Blog</h1>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+
+    <router-view />
+
+    <footer>
+      <p>© 2025 My Vue Blog</p>
+    </footer>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  margin: 0 auto;
+  max-width: 900px;
+  padding: 1rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #eee;
+  margin-bottom: 1rem;
+}
+
+nav a {
+  margin: 0 0.5rem;
+  text-decoration: none;
+  color: #0077ff;
+  font-weight: 500;
+}
+
+nav a.router-link-active {
+  text-decoration: underline;
+}
+
+footer {
+  margin-top: 2rem;
+  border-top: 1px solid #eee;
+  padding-top: 1rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #777;
+  font-size: 0.9rem;
 }
 </style>
